b643ea7ca5539d5bd492c79e78bb23f79ca55d46
petriv03 <154843713+petriv03@users.noreply.github.com>
2024-01-07 11:09:35 +0100

    modif

--- R/customplot.R
+++ R/customplot.R
@@ -1,24 +1,31 @@
 
-# module function =============================================================
+# public function =============================================================
 #' Annotate
 #'
-#' Arrange ggplots into grid and annotate the grid.
-#' @param plotlist list of ggplots
-#' @param title common title
-#' @param xlab common x axis label
-#' @param ylab common y axis label
-#' @param xlim vector of two numeric values delimiting common x axis
-#' @param ylim vector of two numeric values delimiting common y axis
-#' @param ncol number of columns
-#' @return annotated figure
+#' Arranges the ggplots into a grid and annotate it. The number of the grid
+#' columns is user-defined. The number of the rows are recalculated based on
+#' the plotlist length.
+#' @param plotlist a list of the ggplots
+#' @param title a title of an annotated figure; character string
+#' @param xlab x-axis label common for all plots in the grid; character string
+#' @param ylab y-axis label common for all plots in the grid; character string
+#' @param xlim a two-element numeric vector delimiting x-axis; numeric vector
+#' @param ylim a two-element numeric vector delimiting y-axis; numeric vector
+#' @param ncol a number of the columns in the grid; integer
+#' @return the ggplots arranged into an annotated grid
 #' @examples
 #' library(customplot)
 #' p1 <- scatter_plot(iris, "Sepal.Width", "Sepal.Width", color = "Species")
 #' p2 <- scatter_plot(iris, "Sepal.Width", "Sepal.Length", color = "Species")
 #' p3 <- scatter_plot(iris, "Sepal.Width", "Petal.Width", color = "Species")
 #' p4 <- scatter_plot(iris, "Sepal.Width", "Petal.Length", color = "Species")
-#' annotate(list(p1, p2, p3, p4), title = "iris", xlim = c(0, 5), ylim = 0, 8),
-#' xlab = "Sepal.Width", ylab = "floating", ncol = 2)
+#' annotate(plotlist = list(p1, p2, p3, p4),
+#'          title = "iris",
+#'          xlim = c(0.5, 5.1),
+#'          ylim = -0.4, 8.3),
+#'          xlab = "Sepal.Width",
+#'          ylab = "floating",
+#'          ncol = 2)
 #' @export
 annotate <- function(plotlist, title = NULL, xlim = NULL, ylim = NULL,
                      xlab = NULL, ylab = NULL, ncol = 1) {
@@ -36,11 +43,17 @@ annotate <- function(plotlist, title = NULL, xlim = NULL, ylim = NULL,
                                     fig.lab = title, fig.lab.face = "bold")
   return(figure)
 }
+# end function
 
-# module function =============================================================
+# internal function ===========================================================
 #' Between
 #'
-#' Helper function.
+#' Returns a numeric value delimited by an maxima and minima. If the input
+#' value is higher than maxima the function returns maxima. If it is lower than
+#' minima the result is minima. Otherwise the input value is retained.
+#' @param value input numeric value
+#' @param min lower limit
+#' @param max upper limit
 #' @keywords internal
 #' @export
 between <- function(value, min, max) {
@@ -50,46 +63,62 @@ between <- function(value, min, max) {
 }
 # end function
 
-# module function =============================================================
+# public function =============================================================
 #' Scatter Plot
 #'
-#' Customized scatter ggplot
-#' @param data_frame source (data.frame)
-#' @param x column name (character) determining x coordinates
-#' @param y column name (character) determining y coordinates
-#' @param color column name (character) determining point color
-#' @param shape column name (character) determining point shape
-#' @param size column name (character) determining point size
-#' @param label column name (character) determining point labels
-#' @param lines boolean to highlight axis origin
-#' @param angle_text_x angle of tics labels at x-axis; numeric (0, 180, NULL)
-#' @param angle_text_y angle of tics labels at y-axis; numeric (0, 180, NULL)
-#' @param ellipse boolean to show 95 percent t-distribution ellipse
-#' @param point_alpha numeric between 0 and 1 determining point transparency
-#' @param legend character description of legend position.
-#' @param legend_hshift numeric between -1 and 1 determining legend horizontal shift
-#' @param legend_vshift numeric between -1 and 1 determining legend vertical shift
-#' @param legend_box multiple legend orientation; string ("horizontal",
-#' "vertical")
-#' @param legend_direction legend direction; string ("horizontal", "vertical")
-#' @param text_overlap boolean allowing text overlap
-#' @return ggplot
+#' Returns a figure of a customized scatter ggplot.
+#' @param data_frame source data frame
+#' @param x name of the column containing x-coordinates; character string.
+#' The column must be factor or numeric.
+#' @param y name of the column containing y-coordinates; character string.
+#' The column must be factor or numeric.
+#' @param color name of the column for point color values; character string.
+#' The column must be factor or numeric.
+#' @param shape name of the column for point shape values; character string.
+#' The column must be factor.
+#' @param size name of the column for point size values; character string.
+#' The column must be numeric.
+#' @param label name of the column for point labels; character string. The
+#' column must be character.
+#' @param lines if TRUE then origin is highlighted by dashed x- and y-lines
+#' @param x_text_direction axis text x direction; "horizontal", "vertical",
+#' "oblique", NULL
+#' @param y_text_direction axis text y direction; "horizontal", "vertical",
+#' "oblique", NULL
+#' @param ellipse if TRUE and color column is factor then 95-percent
+#' t-distribution ellipses appear around the color groups.
+#' @param point_alpha point transparency; numeric value between 0
+#' (fully transparent) and 1 (fully solid).
+#' @param legend position of a legend; "top", "bottom", "left", "right",
+#' "topleft", "bottomleft", "topright", "bottomright", "center"
+#' @param legend_hshift legend horizontal shift; numeric value between 0
+#' (very left) and 1 (very right).
+#' @param legend_vshift legend vertical shift; numeric value between 0
+#' (very bottom) and 1 (very top).
+#' @param legend_box orientation of multiple legend (if more than one point
+#' proportion is defined); "horizontal", "vertical"
+#' @param legend_direction direction of legend keys; "horizontal", "vertical"
+#' @param legend_key_size legend key size in text lines; numeric value
+#' @param text_overlap if FALSE then point label overlapping is avoided. This
+#' action can take more computation time.
+#' @return customized ggplot
 #' @examples
 #' library(customplot)
-#' # legend: "top", "left", "right", "bottom", "center", "topleft", "topright",
-#' # "bottomleft", "bottomright"
 #' scatter_plot(data_frame = iris, x = "Sepal.Width", y = "Sepal.Length",
 #' color = "Petal.Width", size = "Petal.Length", shape = "Species",
-#' label = "Species", lines = TRUE, legend = "right", xaxt = TRUE,
-#' yaxt = FALSE, ellipse = FALSE, alpha = 0.5, hjust = -0.05, vjust = 0.1)
+#' label = "Species", lines = TRUE, legend = "right", point_alpha = 0.5,
+#' legend_hshift = -0.05, legend_vshift = 0.1)
 #' @export
 scatter_plot <- function(data_frame, x, y, color = NULL, shape = NULL,
                          size = NULL, label = NULL, xlim = NULL, ylim = NULL,
-                         lines = FALSE, legend = "topright", angle_text_x = 0,
-                         angle_text_y = 0, ellipse = FALSE, point_alpha = 1, legend_hshift = 0,
+                         lines = FALSE, legend = "topright",
+                         x_text_direction = "horizontal",
+                         y_text_direction = "horizontal", ellipse = FALSE,
+                         point_alpha = 1, legend_hshift = 0,
                          legend_vshift = 0, text_overlap = TRUE,
                          legend_box = "vertical",
-                         legend_direction = "vertical") {
+                         legend_direction = "vertical",
+                         legend_key_size = 1) {
 
   mapping <- get_mapping(x, y, color, shape, size, label)
   ggplot2::ggplot(data_frame, mapping) +
@@ -103,19 +132,24 @@ scatter_plot <- function(data_frame, x, y, color = NULL, shape = NULL,
     get_ellipse(data_frame[, color], ellipse) +
     get_color_scheme(data_frame[, color]) +
     get_custom_theme(legend = legend,
-                     angle_text_x = angle_text_x,
-                     angle_text_y = angle_text_y,
+                     x_text_direction = x_text_direction,
+                     y_text_direction = y_text_direction,
                      legend_hshift = legend_hshift,
                      legend_vshift = legend_vshift,
                      legend_box = legend_box,
-                     legend_direction = legend_direction)
+                     legend_direction = legend_direction,
+                     legend_key_size = legend_key_size)
 }
 # end function
 
-# module function =============================================================
-#' Get Adjusted Coords
+# internal function ===========================================================
+#' Get Adjusted Coordinates
 #'
-#' Helper function.
+#' Returns an two-element numeric vector of the legend coordinates
+#' @param coordinates two-element numeric vector of input coordinates
+#' @param legend_hshift horizontal shift; numeric value
+#' @param legend_vshift vertical shift; numeric value
+#' @keywords internal
 #' @export
 get_adjusted_coords <- function(coordinates, legend_hshift, legend_vshift) {
   c(between(coordinates[1] + legend_hshift, 0, 1),
@@ -123,36 +157,41 @@ get_adjusted_coords <- function(coordinates, legend_hshift, legend_vshift) {
 }
 # end function
 
-# module function =============================================================
+# internal function============================================================
 #' Get Axis Text
 #'
-#' Helper function.
+#' Returns the text at ggplot axis
+#' @param direction direction of the text; "horizontal", "vertical", "oblique",
+#'  NULL
 #' @keywords internal
 #' @export
-get_axis_text <- function(angle) {
-  if (angle == 90) {
-    gg <- ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
+get_axis_text <- function(direction) {
+  if (is.null(direction)) {
+    ggplot2::element_blank()
+  } else if (direction == "vertical") {
+    ggplot2::element_text(angle = 90, vjust = 0.5, hjust = 1)
+  } else if (direction == "oblique") {
+    ggplot2::element_text(angle = 45, vjust = 1, hjust = 1)
+  } else if (direction == "horizontal") {
+    ggplot2::element_text()
   } else {
-    gg <- ggplot2::element_text()
-  }
-  if (is.null(angle)) {
-    ggplot2::element_blank()  # blank text
-  } else {
-    gg  # normal text
+    stop("get_axis_text: direction is not defined")
   }
 }
 # end function
 
-# module function =============================================================
+# internal function ===========================================================
 #' Get Color Scheme
 #'
-#' Helper function.
+#' Returns a gg color scheme based on input vector class.
+#' @param color_column input vector. If class is factor then color scheme is
+#' 6-point brewer. If class is numeric then color scheme is continuous
+#' three-point gradient with mean value in the middle.
 #' @keywords internal
 #' @export
 get_color_scheme <- function(color_column) {
   if (class(color_column) == "factor") {
     ggplot2::scale_color_brewer(palette = "Set2")
-    #viridis::scale_color_viridis(discrete = TRUE)
   } else if (class(color_column) == "numeric") {
     ggplot2::scale_color_gradient2(low = "#00AFBB",
                                    mid = "#E7B800",
@@ -164,16 +203,29 @@ get_color_scheme <- function(color_column) {
 }
 # end function
 
-# module function =============================================================
+# internal function ===========================================================
 #' Get Custom Theme
 #'
-#' Helper function.
+#' Returns gg theme
+#' @param legend position of a legend; "top", "bottom", "left", "right",
+#' "topleft", "bottomleft", "topright", "bottomright", "center"
+#' @param legend_hshift legend horizontal shift; numeric value between 0
+#' (very left) and 1 (very right).
+#' @param legend_vshift legend vertical shift; numeric value between 0
+#' (very bottom) and 1 (very top).
+#' @param legend_box orientation of multiple legend (if more than one point
+#' proportion is defined); "horizontal", "vertical"
+#' @param legend_direction direction of legend keys; "horizontal", "vertical"
+#' @param legend_key_size legend key size in text lines; numeric value
+#' @param x_text_direction axis text x direction; "horizontal", "vertical",
+#' "oblique", NULL
+#' @param y_text_direction axis text y direction; "horizontal", "vertical",
+#' "oblique", NULL
 #' @keywords internal
 #' @export
-get_custom_theme <- function(legend, angle_text_x, angle_text_y,
-                             legend_hshift,
-                             legend_vshift, legend_box = "vertical",
-                             legend_direction = "vertical") {
+get_custom_theme <- function(legend, legend_hshift, legend_vshift, legend_box,
+                             legend_direction, legend_key_size,
+                             x_text_direction, y_text_direction) {
 
   # plot elements
   bold_text <- ggplot2::element_text(face = "bold")
@@ -187,28 +239,32 @@ get_custom_theme <- function(legend, angle_text_x, angle_text_y,
   custom_theme <- ggplot2::theme_bw() +  # black and white background
     ggplot2::theme(
       text = small_text,  # plot text
-      axis.text.x = get_axis_text(angle_text_x),  # axis text
-      axis.text.y = get_axis_text(angle_text_y),
+      axis.text.x = get_axis_text(x_text_direction),  # axis text
+      axis.text.y = get_axis_text(y_text_direction),
       legend.background = transparent_rect,  # legend properties
       legend.key = transparent_rect,
       legend.justification = legend_coordinates,
       legend.position = legend_coordinates,
       legend.title = bold_text,
       legend.box = legend_box,
-      legend.direction = legend_direction)
+      legend.direction = legend_direction,
+      legend.key.size = ggplot2::unit(legend_key_size, "lines"))
 
   return(custom_theme)
 }
 # end function
 
-# module function =============================================================
+# internal function ===========================================================
 #' Get Ellipse
 #'
-#' Helper function.
+#' If color_column is factor and ellipse is TRUE, the function get_ellipse
+#' returns 95 percent t-distrubution ellipse around the color point group.
+#' @param input_factor an input factor
+#' @param allowed if TRUE then the ellipse appears
 #' @keywords internal
 #' @export
-get_ellipse <- function(color_column, ellipse) {
-  if (class(color_column) == "factor" & ellipse == TRUE) {
+get_ellipse <- function(input_factor, allowed) {
+  if (class(input_factor) == "factor" & allowed == TRUE) {
     ggplot2::stat_ellipse(level = 0.95, type = "t")
   } else {
     NULL
@@ -216,10 +272,16 @@ get_ellipse <- function(color_column, ellipse) {
 }
 # end function
 
-# module function =============================================================
+# internal function =============================================================
 #' Get Legend Coordinates
 #'
-#' Helper function.
+#' Returns two-element numeric vector of legend coordinates.
+#' @param legend position of a legend; "top", "bottom", "left", "right",
+#' "topleft", "bottomleft", "topright", "bottomright", "center"
+#' @param hjust legend horizontal shift; numeric value between 0 (very left)
+#' and 1 (very right).
+#' @param vjust legend vertical shift; numeric value between 0 (very bottom)
+#' and 1 (very top).
 #' @keywords internal
 #' @export
 get_legend_coordinates <- function(legend, hjust, vjust) {
@@ -236,14 +298,15 @@ get_legend_coordinates <- function(legend, hjust, vjust) {
 }
 # end function
 
-# module function =============================================================
+# internal function ===========================================================
 #' Get Lines
 #'
-#' Helper function.
+#' Returns list of gg dashed lines at origin of x- and y-axis.
+#' @param allowed if TRUE then the lines are retained in the output list
 #' @keywords internal
 #' @export
-get_lines <- function(lines) {
-  if (lines) {
+get_lines <- function(allowed) {
+  if (allowed) {
     list(ggplot2::geom_vline(xintercept = 0, linetype = "dashed"),
          ggplot2::geom_hline(yintercept = 0, linetype = "dashed"))
   } else {
@@ -252,12 +315,24 @@ get_lines <- function(lines) {
 }
 # end function
 
-# module function =============================================================
+# internal function ===========================================================
 #' Get Mapping
 #'
-#' Helper function.
+#' Returns gg aes() containing the proportions of a ggplot.
+#' @param x name of the column containing x-coordinates; character string.
+#' The column must be factor or numeric.
+#' @param y name of the column containing y-coordinates; character string.
+#' The column must be factor or numeric.
+#' @param color name of the column for point color values; character string.
+#' The column must be factor or numeric.
+#' @param shape name of the column for point shape values; character string.
+#' The column must be numeric.
+#' @param size name of the column for point size values; character string.
+#' The column must be numeric.
+#' @param label name of the column for point labels; character string. The
+#' column must be numeric.
 #' @keywords internal
-#' @noRd
+#' @export
 get_mapping <- function(x, y, color, shape, size, label) {
 
   blank <- is.null(color) & is.null(shape) & is.null(size) & is.null(label)
@@ -341,10 +416,14 @@ get_mapping <- function(x, y, color, shape, size, label) {
 }
 # end function
 
-# module function =============================================================
+# internal function ===========================================================
 #' Get Label
 #'
-#' Helper function.
+#' Returns gg label element.
+#' @param label name of the column for point labels; character string. The
+#' column must be numeric.
+#' @param overlap if FALSE then point label overlapping is avoided. This
+#' action can take more computation time.
 #' @keywords internal
 #' @export
 get_label <- function(label, overlap) {
@@ -360,10 +439,12 @@ get_label <- function(label, overlap) {
 }
 # end function
 
-# module function =============================================================
+# internal function ===========================================================
 #' Get xlim
 #'
-#' Helper function.
+#' Returns gg xlim
+#' @param xlim two-element numeric vector delimiting x-axis
+#' @keywords internal
 #' @export
 get_xlim <- function(xlim) {
   if (is.null(xlim)) {NULL} else {ggplot2::xlim(xlim)}
@@ -373,7 +454,9 @@ get_xlim <- function(xlim) {
 # module function =============================================================
 #' Get ylim
 #'
-#' Helper function.
+#' Returns gg ylim
+#' @param ylim two-element numeric vector delimiting y-axis
+#' @keywords internal
 #' @export
 get_ylim <- function(ylim) {
   if (is.null(ylim)) {NULL} else {ggplot2::ylim(ylim)}
